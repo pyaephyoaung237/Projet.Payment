9d044e97c845da93cb1b08a184a0a17ac8cc4cb4
pyaephyoaung <pyaephyoaung2377@gmail.com>
2025-01-18 23:07:40 +0630

    file1

---
+++ index.js
@@ -0,0 +1,34 @@
+const mycheckbox =document.getElementById("mycheckbox") ;
+const visabtn =document.getElementById("visabtn") ;
+const paypalbtn =document.getElementById("paypalbtn") ;
+const masterbtn =document.getElementById("masterbtn") ;
+const mysubmit =document.getElementById("mysubmit") ;
+const checkboxresult =document.getElementById("checkboxresult") ;
+const payresult =document.getElementById("payresult") ;
+
+mysubmit.onclick = function (){
+    if(mycheckbox.checked){
+        checkboxresult.textContent = "Your Subcribe now!";
+    }
+      else {
+        checkboxresult.textContent = "Your not subcribe!" ;
+      }
+    if(visabtn.checked){
+        payresult.textContent = "your paying with visa card" ;
+    }
+    else if(paypalbtn.checked){
+        payresult.textContent = "your paying with paypal card" ;
+
+    }
+    else if(masterbtn.checked){
+        payresult.textContent = "your paying with paypal card" ;
+
+    }
+    else {
+        payresult.textContent = "you must be select one payment type!" ;
+    }
+
+
+
+    
+}
